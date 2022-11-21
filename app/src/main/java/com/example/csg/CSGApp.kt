c38bf93ada3957605852cb06ce40bafa07085d4a
roblesdotdev <httpsnip@gmial.com>
2022-11-21 15:18:58 -0300

    basic design system

--- app/src/main/java/com/example/csg/CSGApp.kt
+++ app/src/main/java/com/example/csg/CSGApp.kt
@@ -3,21 +3,21 @@ package com.example.csg
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 import com.example.csg.ui.theme.CSGTheme
 
 @Composable
 fun CSGApp() {
     CSGTheme {
         Surface(
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background,
         ) {
            Column(
                verticalArrangement = Arrangement.Center,
@@ -25,15 +25,14 @@ fun CSGApp() {
            ) {
                 Text(
                     text = "Working...",
-                    fontSize = 24.sp,
-                    fontWeight = FontWeight.Bold,
+                    style = MaterialTheme.typography.h5
                 )
            }
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun CSGAppPreview() {
     CSGApp()
